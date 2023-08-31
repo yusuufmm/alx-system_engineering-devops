72c7089258c2637510ff362d076d9e2b6ac8562d
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2023-08-30 17:26:35 -0700

    ssh cofig

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #using puppet script to create or make changes our ssh configuration file
-file {'ect/ssh/ssh_cofig':
+file { 'ect/ssh/ssh_config':
        ensure => present,
 
 content =>"
