e042f5d525ecd827a6ba43f8f53cbf55af0b4c91
YUSUF MM <109472955+yusuufmm@users.noreply.github.com>
2023-11-04 14:28:08 +0100

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,8 +0,0 @@
-# using puppet to creat a file in /tmp
-file { '/tmp/school':
-    ensure  => 'file',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0744',
-    content => 'I love Puppet',
-}
--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,5 +0,0 @@
-# using Puppet to install flask from pip3
-package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
-}
