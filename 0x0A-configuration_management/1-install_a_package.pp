6122861e79adea2ddb848b78dfa70fbcfa8e6829
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2023-11-24 20:38:21 -0800

    Tasks 1 and 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Using Puppet to install flask from pip3.
-# Task 1
+# Ensure pip3 is installed
+   package { 'python3-pip':
+   ensure => 'installed',
+}
 package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
+    require  => Package['python3-pip'],
+
 }
