f1ac57197a94ef928d2d4168a5ed0967f358dd0c
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2024-06-23 05:32:21 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers"""
+Module to query the number of subscribers on a given Reddit subreddit.
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Function to fetch the number of subscriber7s """
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        return 0
+    except requests.RequestException:
         return 0
